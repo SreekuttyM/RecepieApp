288a311bdb886a6c4d94001559de9fb8cc7e26ee
Sreekutty Maya <sreekuttystar@gmail.com>
2025-02-21 00:03:30 +0400

    resources added - app icon, launch screen
    accent color added
    font added - poppins

--- ReceipeApp/ReceipeApp/ContentView.swift
+++ ReceipeApp/ReceipeApp/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.custom("Poppins-Bold", size: 30))
         }
         .padding()
     }
