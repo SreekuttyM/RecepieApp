49715eb47358c2641c7d08a7a35c06feda028153
Sreekutty Maya <sreekuttystar@gmail.com>
2025-02-20 22:19:41 +0400

    Initial Project Setup

---
+++ ReceipeApp/ReceipeApp/ReceipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipeAppApp.swift
+//  ReceipeApp
+//
+//  Created by Sreekutty Maya on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReceipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReceipeApp/ReceipeAppTests/ReceipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipeAppTests.swift
+//  ReceipeAppTests
+//
+//  Created by Sreekutty Maya on 20/02/2025.
+//
+
+import Testing
+@testable import ReceipeApp
+
+struct ReceipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
